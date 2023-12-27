1012d86be70d27679a6c0c59ff213b5bf0024c88
sharathone23 <sharathone3@gmail.com>
2023-12-26 22:17:47 -0600

    Implement TopologicalSortOrder using Kahn’s Algorithm

--- nonlinear/DirectedGraph.java
+++ nonlinear/DirectedGraph.java
@@ -1,5 +1,10 @@
 package nonlinear;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
 public class DirectedGraph extends Graph{
 
     /**
@@ -32,6 +37,36 @@ public class DirectedGraph extends Graph{
      */
     void printTopologicalSort(){
 
+        //Return if cycle exists, Topological sort is for Acyclic graphs
+        if(isCycleExists()){
+            return;
+        }
+        //Find degree of each vertex
+        Map<Integer, Integer> degreeCount = new HashMap<>();
+        for(GraphNode node: nodes){
+            for(GraphNode neighbour: node.neighbours){
+                degreeCount.put(neighbour.data, degreeCount.getOrDefault(neighbour.data, 0) +1);
+            }
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        //Add all zero degree vertices
+        for(GraphNode node: nodes){
+            if(!degreeCount.containsKey(node.data)){
+                queue.add(node.data);
+            }
+        }
+        while(!queue.isEmpty()){
+            Integer vertex = queue.poll();// Remove one vertex and decrement its neighbours in degree by 1
+            System.out.print(vertex +" ");
+            GraphNode node = findNode(vertex);
+            for(GraphNode neighbour: node.neighbours){
+                degreeCount.put(neighbour.data, degreeCount.get(neighbour.data)-1);
+                if(degreeCount.get(neighbour.data) == 0){
+                    queue.add(neighbour.data); // Add it to queue if in degree count is zero and remove from map.
+                    degreeCount.remove(neighbour.data);
+                }
+            }
+        }
     }
 
     /**
