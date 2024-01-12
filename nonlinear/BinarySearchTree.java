075c84214fc1006fef6d3edfb1fbc0de6193a594
sharathone23 <sharathone3@gmail.com>
2024-01-12 08:56:28 -0600

    Implement delete in a BST and also added methods for insert,search iteratively

--- nonlinear/BinarySearchTree.java
+++ nonlinear/BinarySearchTree.java
@@ -14,13 +14,14 @@ public class BinarySearchTree extends BinaryTree {
      *
      * @param value to be searched in Tree
      * @return Node which matches the given value Or null if Tree is null or not found.
-     * Time Complexity - O(logn) - Binary Search
+     * Time Complexity - O(h) , O(logn) if we maintain a Balanced Search Tree for every insert and delete like AVL and Red Black Trees
      */
     public Node search(int value) {
         if (this.root == null) return null;
-        return find(value, root);
+        return findIterative(value, root);
     }
 
+    // Recursive find - Space Complexity - O(h)
     private Node find(int value, Node node) {
         if (node == null) return null;
         if (node.data == value) return node;
@@ -31,6 +32,20 @@ public class BinarySearchTree extends BinaryTree {
             //search right
             return find(value, node.right);
         }
+    }
+
+    // Iterative find - Space Complexity - O(1)
+    private Node findIterative(int value, Node node) {
+        if (node == null) return null;
+        while(node != null){
+            if(node.data == value) return node;
+            if(node.data < value){
+                node = node.right;
+            }else{
+                node = node.left;
+            }
+        }
+        return null;
     }
 
     /**
@@ -83,7 +98,8 @@ public class BinarySearchTree extends BinaryTree {
 
     /**
      * Inserts a new node with given value at the correct place in the BinarySearchTree. Uses Binary Search.
-     *
+     * Time Complexity - O(h), O(log n) if we maintain a Balanced Search Tree for every insert and delete like AVL and Red Black Trees
+     * Space Complexity - O(h)
      * @param value
      */
     public void insert(int value) {
@@ -93,13 +109,44 @@ public class BinarySearchTree extends BinaryTree {
             return;
         }
         Node parent = findParentForInsert(value, root);
-        if (parent != null) { // This should never be null for non empty tree which is already checked above
+        if (parent != null) { // This should never be null for non-empty tree which is already checked above
             if (parent.data < value) {
                 parent.right = node;
             } else if (parent.data > value) {
                 parent.left = node;
             }
             // Ignore duplicate if already exists
+        }
+    }
+
+    /**
+     * Inserts a new node with given value at the correct place in the BinarySearchTree Iteratively. Uses Binary Search.
+     * Time Complexity - O(h), O(log n) if we maintain a Balanced Search Tree for every insert and delete like AVL and Red Black Trees
+     * Space Complexity - O(1)
+     *
+     * @param value
+     */
+    public void insertIterative(int value){
+        Node newNode = new Node(value);
+        if (root == null){
+            this.root = newNode;
+            return;
+        }
+        Node parent = null;
+        Node current = root;
+        while(current != null){
+            parent = current;
+            if(current.data == value) return; // Ignore if Node with value already exists
+            if(current.data < value){
+                current = current.right;
+            }else{
+                current = current.left;
+            }
+        }
+        if(parent.data < value){
+            parent.right = newNode;
+        }else{
+            parent.left = newNode;
         }
     }
 
@@ -119,7 +166,6 @@ public class BinarySearchTree extends BinaryTree {
 
 
     /**
-     * TODO
      * Deletes a node with given value.
      *
      * @param value
@@ -134,6 +180,31 @@ public class BinarySearchTree extends BinaryTree {
 
          Node with Two Children: Find the node's in-order successor (the smallest node in its right subtree) or in-order predecessor (the largest node in its left subtree), copy its value to the node, and then delete the in-order successor or predecessor.
          */
+        delNode(root, value);
+    }
+
+    private Node delNode(Node root, int value){
+        if(root == null) return null;
+        if(root.data < value){
+            root.right = delNode(root.right, value);
+        }else if(root.data > value){
+            root.left = delNode(root.left, value);
+        } else {
+            if(root.right == null) return root.left;
+            if(root.left == null) return root.right;
+            Node succesorNode = getInOderSuccessorForDelete(root);
+            delNode(succesorNode,value); // deletes the successor node as this is moved to current root
+            root.data = succesorNode.data;
+        }
+        return root;
+    }
+
+    private Node getInOderSuccessorForDelete(Node root){
+        Node curr = root.right;
+        while(curr != null && curr.left != null){
+            curr = curr.left;
+        }
+        return curr;
     }
 
 }
