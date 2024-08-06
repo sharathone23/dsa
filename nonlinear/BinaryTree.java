11330e9ddc86aaa34f80f87be0c241f52dfe30aa
sharathone23 <sharathone3@gmail.com>
2024-08-06 14:02:58 -0500

    cleanup delete methods which causes to loose children of deleted node

--- nonlinear/BinaryTree.java
+++ nonlinear/BinaryTree.java
@@ -188,7 +188,7 @@ public class BinaryTree {
      * Inserts a new node with given value at the end of the BinaryTree. Uses Level Order Traversal(BFS) and finds the next available spot to make it a complete binary tree. 
      * @param value
      */
-    public void insert(int value){
+    public void insertLevelOrder(int value){
         Node node = new Node(value);
         if(root == null) {
             this.root = node;
@@ -213,65 +213,6 @@ public class BinaryTree {
         }
     }
 
-
-    /**
-     * Deletes a node with given value. If deleted node is not a leaf i.e a parent. It makes the last node of tree to be the new parent to make the tree complete
-     * @param value
-     */
-    public void delete(int value){
-        if(root == null) {
-            return;
-        }
-        Node nodeToBeDeleted = null;
-        Node lastNode = null; // This will store the lastNode when below while loop ends
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(root);
-        while(!queue.isEmpty()){
-            lastNode = queue.poll();
-            if(lastNode.data == value){
-                nodeToBeDeleted = lastNode; // we let the loop run here by not adding a break; to find the lastNode of the Tree
-            }else{
-                if(lastNode.left != null) queue.add(lastNode.left);
-                if(lastNode.right != null)queue.add(lastNode.right);
-            }
-        }
-        if(nodeToBeDeleted != null) {
-            nodeToBeDeleted.data = lastNode.data; // override the data in nodeToBeDeleted with lastNodes value 
-            deleteNode(lastNode);//delete lastNodes reference from its parent(left/right).
-        }
-    }
-
-    //Deletes the given node from Tree - by removing the reference from its parent(left/right) and marking as null
-    private void deleteNode(Node node) {
-        if (root == null) {
-            return;
-        }
-
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(root);
-
-        while (!queue.isEmpty()) {
-            Node temp = queue.poll();
-
-            if (temp.left != null) {
-                if (temp.left == node) {
-                    temp.left = null;
-                    return;
-                } else {
-                    queue.add(temp.left);
-                }
-            }
-
-            if (temp.right != null) {
-                if (temp.right == node) {
-                    temp.right = null;
-                    return;
-                } else {
-                    queue.add(temp.right);
-                }
-            }
-        }
-    }
 
 
     /**
